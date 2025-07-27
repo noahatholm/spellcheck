ff5c2e152b195a28030e56fe1a56b05aff081d12
Noah Holm <noahatholm@gmail.com>
2025-07-28 00:07:44 +0100

    implemented fuzzy search to find nearest neighbours

--- src/trie.py
+++ src/trie.py
@@ -1,14 +1,7 @@
 import time
 from cleaning import normaliseFile
+import sys
 
-def timed(fn):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = fn(*args, **kwargs)
-        end = time.time()
-        print(f"{fn.__name__} took {end - start:.6f} seconds")
-        return result
-    return wrapper
 
 
 class TrieNode:
@@ -47,13 +40,6 @@ class TrieNode:
     
 TOTALCHARS = 27
 
-def pos(char): #Helper function that returns the positon of the array any char should be placed in
-    #Special Character Support
-    if char == "'":
-        return 26
-
-    #Normal Characters
-    return ord(char.lower()) - 97
 
 class Trie:
     def __init__(self):
@@ -66,26 +52,32 @@ class Trie:
         #Check if node exists in head
         char = word[0]
         if self.head[pos(char)] == None:
-            self.head[pos(char)] = TrieNode(char,False)
+            self.head[pos(char)] = TrieNode(char,len(word) == 1) #Sets end of word to true if len word is a char
 
+
+        if len(word) == 1:
+            return #already done added from above
+        
         end = len(word) - 1
         node = self.head[pos(char)]
-        for i,char in enumerate(word):
-            
-            node = node.addChild(char,i == end)
-            #print(node) 
+        for i in range(1, len(word)):
+                node = node.addChild(word[i], (i == end))
             
     
     def findWord(self,word):
         if word == "":
             return True
         
+        word = word.lower()
+
         node = self.head[pos(word[0])] #If Root node doesnt exist
         if node == None:
             return False
 
+        if len(word) == 1:
+            return word == node.getValue()
 
-        i = 0
+        i = 1
         end = len(word)
         while i < end and node.getChild(word[i]) != None:
             node = node.getChild(word[i])
@@ -93,7 +85,7 @@ class Trie:
 
         return node.isEnd() and (i == (end))
     
-    @timed
+
     def displayTrie(self): #Performs Dfs on the trie to return a list of all the words in the trie
         results = []
 
@@ -103,7 +95,7 @@ class Trie:
                 #print(stack)
                 node,pathToNode = stack.pop()
                 if node.isEnd():
-                    results.append("".join(map(TrieNode.getValue,pathToNode)))
+                    results.append(root.getValue() + "".join(map(TrieNode.getValue,pathToNode)))
                 for childNode in node.getChildren():
                     if childNode != None:
                         stack.append((childNode, pathToNode + [childNode]))
@@ -113,26 +105,69 @@ class Trie:
             if head:
                 dfs(head)   
         return results
+    
+
+
+    def fuzzySearch(self,word, maxDistance):
+        #distance row
+        intialRow = list(range(len(word)+1))
+        results = []
+        #print(prevRow)
+
+        for node in self.head: #Iterate through the head's children and runs a FuzzySearch on each one thats not None
+            if node: #check if node is not none
+                currentRow = leveinsteinDistance(intialRow,node.getValue(), word)
+                if min(currentRow) <= maxDistance:
+                    recursiveFuzzySearch(node,word,maxDistance,currentRow,node.getValue(),results)
+        return results
+
+    def addFromFile(self, filename:str):
+        path = "..//corpus//dictionary//english//"+filename
+        f = open(normaliseFile(path),encoding='utf-8')
+        i = 0
+        for word in f:    
+            word = word[:-1]
+            i+=1
+            self.addWord(word)
+        print(f"Successfully added {i} Words") 
+
+
+def pos(char): #Helper function that returns the positon of the array any char should be placed in
+    #Special Character Support
+    if char == "'":
+        return 26
+
+    #Normal Characters
+    return ord(char.lower()) - 97
+
+def leveinsteinDistance(prevRow, trie_char, word): #Calculates the number of changes to change the current path to target word and then returns the minimum changes
+    currentRow = [prevRow[0] + 1]
+    for j in range(1, len(word) + 1):
+        insertion = currentRow[j - 1] + 1
+        deletion = prevRow[j] + 1
+        substitution = prevRow[j - 1] + (0 if trie_char == word[j - 1] else 1)
+        currentRow.append(min(insertion, deletion, substitution))
+    return currentRow
+
+
+def recursiveFuzzySearch(node: TrieNode, word: str, maxDistance: int, prevRow: list, prefix: str, results: list): #types provided for intellisense
+    if node.isEnd() and prevRow[-1] <= maxDistance:
+        results.append((prefix, prevRow[-1]))
+    #
+    for child in node.getChildren():
+        if child:
+            char = child.getValue()
+            currRow = leveinsteinDistance(prevRow, char, word)
+            if min(currRow) <= maxDistance:
+                recursiveFuzzySearch(child, word, maxDistance, currRow, prefix + char, results)
 
 
 
-   
-
-
-        
-
+ 
     
             
     
-def addFromFile(tree:Trie, filename:str):
-    path = "..//corpus//dictionary//english//"+filename
-    f = open(normaliseFile(path),encoding='utf-8')
-    i = 0
-    for word in f:    
-        word = word[:-1]
-        i+=1
-        tree.addWord(word)
-    print(f"Successfully added {i} Words")
+
 
 
 
@@ -145,15 +180,23 @@ def test():
     # print(Node.getChildren())
     # repr(Node)
     tree = Trie()
-    # tree.addWord("bee")
-    # tree.addWord("b")
-    ##tree.addWord("Sexyback")
-    tree.addWord("didn't")
-    #print(tree)
-    print(tree.findWord("didn't"))
-    
-    addFromFile(tree,"en_GB-large.txt")
 
+    
+    tree.addFromFile("en_GB-large.txt")
+
+    #Test Fuzzy Search
+    # tree.addWord("cab")
+    # tree.addWord("cat")
+    # tree.addWord("cart")
+    # tree.addWord("cut")
+    # tree.addWord("dog")
+    #tree.addWord("c")
+    print(tree.findWord("noah"))
+    #print(tree.findWord("zippier"))
+
+    #print(tree.displayTrie())
+
+    print(tree.fuzzySearch("noha",1))
 
 
 
